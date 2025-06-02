acc02a9e94d5b1a3694cc4b20efe1c88bcf77728
James romanov <qahramonovjmashid1@gmail.com>
2025-06-02 16:48:39 +0500

    done i guess

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(session());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  console.log(`🚀 App running`);
 }
 bootstrap();
